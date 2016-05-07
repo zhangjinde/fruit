be4fbf3be9f91e3138f09e84c25a9c839a2673fd
hah <1210383806@qq.com>
2016-05-07 16:40:11 +0800

    评论图标

--- src/components/detial/BlockCmt.js
+++ src/components/detial/BlockCmt.js
@@ -3,9 +3,19 @@ import React, { Component } from 'react'
 export default class BlockCmt extends Component{
   render() {
     return (
-      <div className="block cmt">
-        <p><img src=""/></p>
-        <p onClick={this.props.showCmt}>查看评价</p>
+      <div className="block cmt clearfix">
+        <p>
+          <a onClick={this.props.showCmt}>
+            <i className="fa fa-commenting-o"></i>
+            查看评价
+          </a>
+        </p>
+        <p>
+          <a href="tel:17002102408">
+            <i className="fa fa-mobile" aria-hidden="true"></i>
+            电话咨询
+          </a>
+        </p>
       </div>
     )
   }
