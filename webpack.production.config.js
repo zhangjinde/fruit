9c2e077a577dff67c2507b7a4125654352a5eb18
tiewei <846173247@qq.com>
2016-10-15 20:27:03 +0800

    生存环境配置

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,15 +4,12 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var extractSCSS = new ExtractTextPlugin('style.css')
 
 module.exports = {
-  //devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
   },
   externals: {
     'react':'React',
@@ -21,7 +18,13 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    extractSCSS
+    extractSCSS,
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),    
+   new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
