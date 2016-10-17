8a9fa958c5a050e9204291b939dd80ad7de23c35
tiewei <846173247@qq.com>
2016-10-17 21:40:35 +0800

    loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,8 @@ export default class Loading extends Component{
   render() {
     return (
       <p className='loading all-center'>
-        <img src="/img/load.gif" className="all-center"/>
+        <img src="/img/load2.png" className="all-center" style={{width:'100px',height:'100px'}}/>
+        <img src="/img/load1.png" className="all-center moving" style={{width:'120px',height:'120px',zIndex:9}}/>
       </p>
     )
   }
